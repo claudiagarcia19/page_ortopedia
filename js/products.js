16605e7d3c6f524b2a62f4cb7ea7e5426fbdb450
Claudia Garcia Dos Santos <claudiacoderhouse@gmail.com>
2023-02-15 14:17:49 -0300

    primer commit del nuevo proyecto ortopedia

---
+++ js/products.js
@@ -0,0 +1,31 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Bota",
+      precio: 35000,
+      img:"/img/bota_walker.png",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Baston",
+      precio: 32000,
+      img:"/img/baston_silfab.png",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Muleta",
+      precio: 10000,
+      img:"/img/muleta_dema.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Sugetador_rodilla",
+      precio: 20000,
+      img:"/img/sugetador_de_rodilla.jpg",
+      cantidad: 1
+    },
+  ];
+  
