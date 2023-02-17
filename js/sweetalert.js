c99c8d02bed8dd9d89b5305adf1bb7b96193f259
Claudia Garcia Dos Santos <claudiacoderhouse@gmail.com>
2023-02-17 17:52:54 -0300

    agregando libreria sweetAlert

---
+++ js/sweetalert.js
@@ -0,0 +1,18 @@
+( async ()=>{
+    const {value: comprar} = await swal.fire ({
+    });
+    if (comprar){
+        alert("para alquiler comunicarse al whatsapp 54 9 1165429404")
+    }  
+})()
+
+
+swal.fire ({
+    title: "Bienvenido!",
+    text: "Estamos felices de que nos visites",
+    
+
+    
+
+})
+
